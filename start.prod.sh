a02d0f419a249442bb97e67064ffa0c5d29bd5ff
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-07-18 17:36:02 +0300

    idk

--- start.prod.sh
+++ start.prod.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # Функция для пуллинга репозитория
+clone_if_not_exists() {
+    local DIR=$1
+    shift
+    local CMD=$@
+
+    if [ ! -d "$DIR" ]; then
+        $CMD
+    fi
+}
+
 pull_repo() {
     local repo_dir=$1
     if [ -d "$repo_dir" ]; then
@@ -11,12 +21,21 @@ pull_repo() {
     fi
 }
 
+
+clone_if_not_exists "chat-boty-backend" "git clone --branch main git@github.com:Alexey-zaliznuak/chat-boty-backend.git"
+clone_if_not_exists "chat-boty-client" "git clone --branch main git@github.com:maxi-q/chat-boty-client.git"
+
+
 echo "Pull gateway repository"
 git pull
 
+
+echo "Clone backend repository if not exists"
 pull_repo "chat-boty-backend"
 
+echo "Clone client repository if not exists"
 pull_repo "chat-boty-client"
+
 
 echo "Stopping and removing existing Docker containers"
 docker compose down
